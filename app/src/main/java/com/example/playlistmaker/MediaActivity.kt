c783d5b59b31e78421b4deb7ecc642b574c7de9e
Zheludock <chepurnykhmv@gmail.com>
2025-01-19 17:18:18 +1000

    need to test

--- app/src/main/java/com/example/playlistmaker/MediaActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MediaActivity.kt
@@ -10,6 +10,5 @@ class MediaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_media)
-
     }
 }
