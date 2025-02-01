f7d11baaa0a40622a3d64b183f5d881d49fc5e24
Zheludock <chepurnykhmv@gmail.com>
2025-02-01 20:49:29 +1000

    except for themes

--- app/src/main/java/com/example/playlistmaker/FindActivity.kt
+++ app/src/main/java/com/example/playlistmaker/FindActivity.kt
@@ -1,14 +1,59 @@
 package com.example.playlistmaker
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class FindActivity : AppCompatActivity() {
+    private var savedText = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_find)
+        val backButton = findViewById<TextView>(R.id.search_top_bar)
+        backButton.setOnClickListener {
+            val backIntent = Intent(this, MainActivity::class.java)
+            startActivity(backIntent)
+        }
+        val searchEditText = findViewById<EditText>(R.id.find_music)
+        val clearButton = findViewById<ImageView>(R.id.clear_button)
+
+        searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        fun hideKeyboard(view: View) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+
+        clearButton.setOnClickListener {
+            searchEditText.text.clear()
+            searchEditText.clearFocus()
+            hideKeyboard(searchEditText)
+        }
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString("savedText", savedText)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        savedText = savedInstanceState.getString("savedText", "") ?: ""
+        val searchEditText = findViewById<EditText>(R.id.find_music)
+        searchEditText.setText(savedText)
     }
 }
